9c3319f28c823e7dae639dbd54c7bd777270c680
wangxianyou <303681285@qq.com>
2019-03-06 17:01:36 +0800

    自定义viewgroup

---
+++ app/src/main/java/com/wxy/wxyandroidstudy/lowerlevel/thefirstpass/test2/custom/CustomViewGroup.java
@@ -0,0 +1,85 @@
+package com.wxy.wxyandroidstudy.lowerlevel.thefirstpass.test2.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CustomViewGroup extends ViewGroup {
+    public CustomViewGroup(Context context) {
+        this(context, null);
+    }
+
+    public CustomViewGroup(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int childCount = getChildCount();
+        if (childCount == 0) {
+            setMeasuredDimension(0, 0);
+        } else {
+
+            if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+                int width = setWidth(childCount);
+                int height = setHeight(childCount);
+                setMeasuredDimension(width, height);
+            }else if(widthMode == MeasureSpec.AT_MOST){
+                int width = setWidth(childCount);
+                setMeasuredDimension(width,heightSize);
+            }else if(heightMode == MeasureSpec.AT_MOST){
+                int height = setHeight(childCount);
+                setMeasuredDimension(widthSize,height);
+            }
+        }
+
+
+    }
+
+    private int setHeight(int childCount) {
+        int height = 0;
+        for (int i = 0; i < childCount; i++) {
+            View childAt = getChildAt(i);
+            height += childAt.getMeasuredHeight();
+        }
+        return height;
+    }
+
+    private int setWidth(int childCount) {
+        int maxWidth = 0;
+        for (int i = 0; i < childCount; i++) {
+            View childAt = getChildAt(i);
+            if (childAt.getWidth() > maxWidth) {
+                maxWidth = childAt.getMeasuredWidth();
+            }
+        }
+        return maxWidth;
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+        int childCount = getChildCount();
+        int height = i1;
+        for (int j = 0; j < childCount; j++) {
+            View childAt = getChildAt(j);
+            int measuredWidth = childAt.getMeasuredWidth();
+            int measuredHeight = childAt.getMeasuredHeight();
+            childAt.layout(i,height,i+measuredWidth,height+measuredHeight);
+            height += measuredHeight;
+        }
+    }
+
+}
--- app/src/main/java/com/wxy/wxyandroidstudy/lowerlevel/thefirstpass/test2/custom/TestViewSpuare.java
+++ app/src/main/java/com/wxy/wxyandroidstudy/lowerlevel/thefirstpass/test2/custom/TestViewSpuare.java
@@ -1,48 +1,79 @@
 package com.wxy.wxyandroidstudy.lowerlevel.thefirstpass.test2.custom;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import com.wxy.wxyandroidstudy.R;
 
 import androidx.annotation.Nullable;
 
 /**
  * 宽度设置为match_parent时，宽度长于高度，不是长方形
  */
+//todo 1.父view是RelativeLayout时不是正方形  2. onMeasure执行多次
 public class TestViewSpuare extends View {
+
+    private int defaultSize;
+
     public TestViewSpuare(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public TestViewSpuare(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public TestViewSpuare(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.TestViewSpuare);
+        defaultSize = array.getDimensionPixelSize(R.styleable.TestViewSpuare_defaultSize, 100);
+        array.recycle();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int withSize = getSize(widthMeasureSpec);
-        int heightSize = getSize(heightMeasureSpec);
+        int withSize = getMySize(widthMeasureSpec, "宽");
+        int heightSize = getMySize(heightMeasureSpec, "高");
         if (withSize < heightSize) {
-            heightSize = withSize;
+            Log.e("www", "onMeasure: withSize = " + withSize + "--heightSize=" + heightSize);
+            setMeasuredDimension(withSize, withSize);
         } else {
-            withSize = heightSize;
+            Log.e("www", "onMeasure: withSize = " + withSize + "--heightSize=" + heightSize);
+            setMeasuredDimension(heightSize, heightSize);
         }
-        setMeasuredDimension(withSize, heightSize);
     }
 
-    public int getSize(int measureSpec) {
-        int result = 100;
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int r = getMeasuredWidth() / 2;
+        Log.e("www", "onDraw: r = "+r);
+        int x = r;
+        int y = r;
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        canvas.drawCircle(x, y, r, paint);
+    }
+
+    public int getMySize(int measureSpec, String str) {
+        int result = defaultSize;
         int size = MeasureSpec.getSize(measureSpec);
         int mode = MeasureSpec.getMode(measureSpec);
+        Log.e("www", "getSize: size = " + size + "---mode = " + mode + "--defaultSize" + defaultSize + "-------" + str);
         switch (mode) {
             case MeasureSpec.EXACTLY:
-            case MeasureSpec.AT_MOST:
                 result = size;
+                break;
+            case MeasureSpec.AT_MOST:
+                result = defaultSize;
                 break;
             default:
                 break;
